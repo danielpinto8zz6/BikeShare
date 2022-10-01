6a16f6559ac89a7c3dc244efc5bd6b906da19475
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2022-10-01 14:26:04 +0100

    clean

--- dock-internal-service/dock.py
+++
@@ -1,9 +0,0 @@
-from tinydb import TinyDB, Query
-
-db = TinyDB('dock.json')
-
-db.insert({'id': '3fa85f64-5717-4562-b3fc-2c963f66afa6', 'port': 11, 'bikeId': '3fa85f64-5717-4562-b3fc-2c963f66afa6'})
-
-Dock = Query()
-dock = db.search(Dock.id == '3fa85f64-5717-4562-b3fc-2c963f66afa6')[0]
-print(dock)
